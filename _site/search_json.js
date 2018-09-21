ed8ccb754753c1b33ec8976f3251c23f46502700
liwenrun <liwenrunWj65423@touna.cn>
2018-09-21 19:16:02 +0800

    add

---
+++ _site/search_json.js
@@ -0,0 +1,105 @@
+window.ydoc_plugin_search_json = {
+  "documents": [
+    {
+      "title": "ydoc",
+      "content": "This is home page of documents book.",
+      "url": "\\documents\\index.html",
+      "children": []
+    },
+    {
+      "title": "",
+      "content": "",
+      "url": "\\documents\\about.html",
+      "children": []
+    },
+    {
+      "title": "组件颜色",
+      "content": "",
+      "url": "\\documents\\color.html",
+      "children": [
+        {
+          "title": "一、颜色（文字、按钮）",
+          "url": "\\documents\\color.html#一、颜色（文字、按钮）",
+          "content": "一、颜色（文字、按钮）ui组件使用的颜色预览、想要修改组件颜色，可以通过修改全局颜色配置variable.sass中的值修改。"
+        },
+        {
+          "title": "二、可以单独引入css",
+          "url": "\\documents\\color.html#二、可以单独引入css",
+          "content": "二、可以单独引入css"
+        },
+        {
+          "title": "三、基本颜色",
+          "url": "\\documents\\color.html#三、基本颜色",
+          "content": "三、基本颜色"
+        },
+        {
+          "title": "主色",
+          "url": "\\documents\\color.html#三、基本颜色-主色",
+          "content": "主色\t.colorWrap{\n\t\toverflow: hidden;\n\t}\n\t.colorItem{\n\t\twidth: 200px;\n\t\tfloat: left;\n\t\tborder-radius: 3px;\n\t}\n\t.colorItem span{\n\t\tcolor: #fff;\n\t}\n\t.colorBox{\n\t\twidth: 140px;\n\t\theight: 60px;\n\t\tdisplay: table-cell;\n    \tvertical-align: bottom;\n\t}\n\t\n\t\t\n\t\t\t#1c81ff\n\t\t\n\t\t\n\t\t\t按钮\n\t\t\t文字触动\n\t\t\n\t\n\t\n\t\t\n\t\t\t#0c2d5f\n\t\t\n\t\t\n\t\t\t图标颜色导航文字触动色标题文字触动色\n\t\t\n\t\n\t\n\t\t\n\t\t\t#ff823e\n\t\t\n\t\t\n\t\t\t利率 、数字特别强调文字标签色、提示\n\t\t\n\t\n"
+        },
+        {
+          "title": "辅助色",
+          "url": "\\documents\\color.html#三、基本颜色-辅助色",
+          "content": "辅助色\t\n\t\t\n\t\t\t#6993ff\n\t\t\n\t\t\n\t\t\t首页合规系列icon 标签\n\t\t\n\t\n\t\n\t\t\n\t\t\t#ffb72b\n\t\t\n\t\t\n\t\t\t标签色 账户中心投资分类\n\t\t\n\t\n\t\n\t\t\n\t\t\t#ff823e\n\t\t\n\t\t\n\t\t\t利率 、数字 特别强调文字 标签色、提示\n\t\t\n\t\n"
+        },
+        {
+          "title": "中性色",
+          "url": "\\documents\\color.html#三、基本颜色-中性色",
+          "content": "中性色\t\n\t\t\n\t\t\t#6993ff\n\t\t\n\t\t\n\t\t\t首页合规系列icon 标签\n\t\t\n\t\n\t\n\t\t\n\t\t\t#ffb72b\n\t\t\n\t\t\n\t\t\t标签色 账户中心投资分类\n\t\t\n\t\n\t\n\t\t\n\t\t\t#ff823e\n\t\t\n\t\t\n\t\t\t利率 、数字 特别强调文字 标签色、提示\n\t\t\n\t\n\t\n\t\t\n\t\t\t#ff823e\n\t\t\n\t\t\n\t\t\t站位文字\n\t\t\n\t\n\t\n\t\t\n\t\t\t#ff823e\n\t\t\n\t\t\n\t\t\t分割线\n\t\t\n\t\n\t\n\t\t\n\t\t\t#ff823e\n\t\t\n\t\t\n\t\t\tweb背景色\n\t\t\n\t\n"
+        }
+      ]
+    },
+    {
+      "title": "字体",
+      "content": "web微软雅黑作为中文字体， din作为阿拉伯数字字体。web解析最小字号12px。英文优选字体：Helvetica Neue",
+      "url": "\\documents\\font.html",
+      "children": [
+        {
+          "title": "使用",
+          "url": "\\documents\\font.html#使用",
+          "content": "使用全局font-family设置采用：font-family: -apple-system,SF UI Display,Arial,PingFang SC,Hiragino Sans GB,Microsoft YaHei,WenQuanYi Micro Hei,Helvetica Neue;"
+        },
+        {
+          "title": "字体大小",
+          "url": "\\documents\\font.html#字体大小",
+          "content": "字体大小lib.scss中设置基础类名.f0、.f12、.f13、.f14、.f16、.f18、.f20、.f24、.f28更多基础类请查看lib.css"
+        },
+        {
+          "title": "示例",
+          "url": "\\documents\\font.html#字体大小-示例",
+          "content": "示例\t.textSample{text-indent: 1em;}\n\tf28\n\tf24\n\tf20\n\tf18\n\tf16\n\tf14\n\tf13\n\tf12\n\tf0\n"
+        }
+      ]
+    },
+    {
+      "title": "",
+      "content": "",
+      "url": "\\documents\\btn.html",
+      "children": []
+    },
+    {
+      "title": "",
+      "content": "",
+      "url": "\\documents\\input.html",
+      "children": []
+    },
+    {
+      "title": "",
+      "content": "",
+      "url": "\\documents\\serch.html",
+      "children": []
+    },
+    {
+      "title": "",
+      "content": "",
+      "url": "\\documents\\textarea.html",
+      "children": []
+    },
+    {
+      "title": "",
+      "content": "",
+      "url": "\\documents\\table.html",
+      "children": []
+    }
+  ]
+}
---
+++ docs/documents/SUMMARY.md
@@ -0,0 +1,13 @@
+# Summary
+
+* [关于](about.md)
+
+### 组件
+
+* [颜色](color.md#)
+* [文字](font.md#)
+* [按钮](btn.md#)
+* [文本框](input.md#)
+* [搜索框](serch.md#)
+* [文本域](textarea.md#)
+* [表格](table.md)
